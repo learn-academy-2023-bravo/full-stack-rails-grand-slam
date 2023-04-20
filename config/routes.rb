f89a06a35da105647f1e7d1d3920dbc209436c76
DeeMo44 <demario326@gmail.com>
2023-04-20 14:19:17 -0400

    complete story 4 blog create

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'blogs' => 'blog#index', as:'blogs'
   get 'blog/:id' => 'blog#show', as:'blog'
   get 'blogs/new' => 'blog#new', as: 'new_blog'
+  post 'blogs' => 'blog#create'
 
-  root 'blog#index'
+  root to: 'blog#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_04_19_215828) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql" 
+  enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
